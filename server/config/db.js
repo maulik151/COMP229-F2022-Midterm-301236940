16d7aa5f1e015335154b53997cb6fa68e20bf66d
parita15 <koladiyamaulik@gmail.com>
2022-11-02 18:53:28 -0400

    Completed edit,add and delete book

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://127.0.0.1:27017/midterm"
+  "URI": "mongodb+srv://maulik:M@ulik151@cluster0.90narqf.mongodb.net/book?retryWrites=true&w=majority"
+};
